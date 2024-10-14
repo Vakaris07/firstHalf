c6a2505246908f352b34e07b6c37ace1b7e72fbe
unknown <aleksandravicius.vakaris07@gmail.com>
2024-10-14 16:16:06 +0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static String firstHalf(String str) {
+        return str.substring(0,str.length()/2);
+
+        }
+        public static void main(String[] args) {
+        System.out.println(firstHalf("WooHoo"));
+        System.out.println(firstHalf("HelloThere"));
+        System.out.println(firstHalf("abcdef"));
+        }
+    }
